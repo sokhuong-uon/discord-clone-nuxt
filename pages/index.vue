9c6d9944b73f11f08368c8c139b932ad1f2226a1
Sokhuong Uon <sokhuong.usk@gmail.com>
2022-12-16 23:55:15 +0700

    chore: init

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="w-screen h-screen bg-teal-600 flex items-center justify-center">
+		Discord!
+	</div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./components/**/*.{vue,ts}", "./pages/*.vue"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
